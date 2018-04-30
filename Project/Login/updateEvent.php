654d047487283de5b89de54e593ca183a116444e
PhatStacks <luongphat7@gmail.com>
2018-04-30 18:39:50 -0500

    Add files via upload

---
+++ Project/Login/updateEvent.php
@@ -0,0 +1,216 @@
+<?php
+
+session_start();
+
+if ($_SESSION['validUser'] == "yes") {
+
+include 'connection.php';
+
+//variables to filled from form
+$updateId = $_GET['event_id'];
+$eventName = "";
+$eventDescription = "";
+$eventPresenter = "";
+$eventDate = "";
+$eventTime = "";
+//variables to hold error messages
+$eventNameError = "";
+$eventDescriptionError = "";
+$eventPresenterError = "";
+$eventDateError = "";
+$eventTimeError = "";
+//variable to determine if form is valid( initially set to "false" to return an invalid (initally blank) form)
+$validForm = false;
+//form validation functions
+
+
+/*$verify_url = 'https://www.google.com/recaptcha/api/siteverify';
+$args = array('secret' => '6LeAIFYUAAAAACPowlK1PHPty644Jw36gFTdXeKY',
+'response' => $_POST['g-recaptcha'],
+'remoteip' => $_SERVER['REMOTE_ADDR']);
+$request_url = $verify_url.'?'.http_build_query($args);
+
+// a JSON object is returned
+$response = file_get_contents($request_url);
+
+// decode the information
+$json = json_decode($response, true); // true decodes it to an array instead of a PHP object
+
+
+
+// handle the response
+if($recaptcha['success'] == 1) {
+// run code on successful reCAPTCHA
+} else {
+// run code on unsuccessful reCAPTCHA
+}*/
+
+
+
+function validateName() //Event Name must be included
+{
+	global $eventName, $eventNameError, $validForm;
+
+	if($eventName == "")
+	{
+		$validForm = false;
+		$eventNameError = "Please enter the name of the event.<br>";
+	}
+}
+function validateDescription() //Event Description must be included
+{
+	global $eventDescription, $eventDescriptionError, $validForm;
+
+	if($eventDescription == "")
+	{
+		$validform = false;
+		$eventDescriptionError = "Please enter a description of the event.<br>";
+	}
+}
+function validatePresenter() //Event Presenter must be included
+{
+	global $eventPresenter, $eventPresenterError, $validForm;
+
+	if($eventPresenter =="")
+	{
+		$validForm = false;
+		$eventPresenterError = "Please enter the name of the event presenter.<br>";
+	}
+}
+function validateDate() //Event Date validation
+{
+	global $eventDate, $eventDateError, $validForm;
+
+	if ($eventDate == " ")
+	{
+		$validForm = false;
+		$eventDateError = "Please enter your event date.<br>";
+	}		
+}
+function validateTime() //Event Start Time must be in hh:mm 
+{
+	global $eventTime, $eventTimeError, $validForm;
+
+
+	if($eventTime == "" )
+	{
+		$validForm = false;
+		$eventTimeError = "Please enter your event time.<br>";
+	}
+}
+//function to determine if the "submit" button has been pressed (a form has been submitted)
+if(isset($_POST["submit"]))
+{
+//fill variables from form
+	$eventName = $_POST["eventName"];
+	$eventDescription = $_POST["eventDescription"];
+	$eventPresenter = $_POST["eventPresenter"];
+	$eventDate = $_POST["eventDate"];
+	$eventTime = $_POST["eventTime"];
+//assume the form is valid before validating
+	$validForm = true;
+
+
+//validate form
+	validateName();
+	validateDescription();
+	validatePresenter();
+	validateDate();
+	validateTime();
+
+
+	$stmt = $conn->prepare("UPDATE wdv341_event SET event_date =:eventDate, event_description =:eventDescription, event_name =:eventName, event_presenter =:eventPresenter, event_time =:eventTime WHERE event_id = '$updateId' ");
+	$stmt->bindParam(':eventName', $eventName);
+	$stmt->bindParam(':eventDescription', $eventDescription);
+	$stmt->bindParam(':eventPresenter', $eventPresenter);
+	$stmt->bindParam(':eventDate', $eventDate);
+	$stmt->bindParam(':eventTime', $eventTime);
+
+	$stmt->execute();
+	echo "<h2>Event Updated Inserted Correctly!</h2>";
+	include 'selectEvents.php';
+
+}
+
+if($updateId == $updateId)
+	$stmt = $conn->query("SELECT * FROM `wdv341_event` WHERE event_id = '$updateId' ");
+while ($row = $stmt->fetch(PDO:: FETCH_ASSOC))
+	{
+
+		$eventName = $row['event_name'];
+		$eventDescription = $row['event_description'];
+		$eventPresenter = $row['event_presenter'];
+		$eventDate = $row['event_date'];
+		$eventTime = $row['event_time'];
+	}
+
+	$conn = null;
+
+}
+else {
+	header('location: login.php');
+}
+
+
+	?>
+
+	<!DOCTYPE html>
+	<html>
+	<head>
+
+		<title>WDV 341: Form Page for Events</title>
+
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+		<style>
+
+		#container  {
+			width:600px;
+			background-color:#CF9;
+		}
+
+		.error  {
+			color:red;
+			font-style:italic;  
+		}
+	</style>
+
+</head>
+<body>
+
+
+
+	<!--Form with Event Name, Description, Presenter, Date, and Time -->
+	<div id="container">
+		<h1>Event Id: <?php echo $updateId?></h1>
+
+		<form name="form1" method="post" action="updateEvent.php?id=<?php echo$updateId?>">
+			<p>Event Name:<br> <span class="error"><?php echo$eventNameError?></span><input type="text" name="eventName" id="eventName" value="<?php echo$eventName?>"></p>
+
+			<p>Event Description:<br><span class="error"><?php echo$eventDescriptionError?></span><textarea name="eventDescription" id="eventDescription" cols="45" rows="5"><?php echo$eventDescription?></textarea></p>
+
+
+			<p>Event Presenter:<br><span class="error"><?php echo$eventPresenterError?></span><input type="text" name="eventPresenter" id="eventPresenter" value="<?php echo$eventPresenter?>"></p>
+
+			<p>Event Date (yyyy-mm-dd):<br><span class="error"><?php echo$eventDateError?></span><input type="date" name="eventDate" id="eventDate" value="<?php echo$eventDate?>"></p>
+
+			<p>Event Start Time (hh:mm):<br><span class="error"><?php echo$eventTimeError?></span><input type="time" name="eventTime" id="eventTime" value="<?php echo$eventTime?>"></p>
+
+			<p><a href="selectEvents.php"> View all events.</a></p>
+
+				<!--<div class="g-recaptcha" data-sitekey="6LeAIFYUAAAAACVyI_xUk1ePfihhR4Ka0ODG-syk"></div>-->
+
+
+				<p><input type="submit" name="submit" value="Update information"></p>
+				<p><input type="reset" name="reset" value="Reset Information"></p>
+
+
+
+
+			</form>
+		</div>
+
+	</body>
+	</html>
---
+++ Project/startbootstrap-freelancer-master/index.php
@@ -0,0 +1,210 @@
+<?php
+
+  session_start();
+  $inUsername = $_SESSION['userName']; //get user name
+
+  //no valid user needed, everyone has acces to this page
+
+  if($inUsername != '') {
+    $messageSuccess = "Welcome Back<em>". $inUsername."</em> !"; //if admin is logged in then display welcome message with user name.
+
+  }
+  else{
+    $messageSuccess =" "; 
+  }
+
+  require 'connection.php';
+  $displayErrorMsg = "";
+
+  try {
+      $sql = $conn->prepare("SELECT product_id, product_name, product_description, product_price FROM project");
+      $sql->execute();
+      $count = $sql->rowCount();
+  }
+  catch(PDOException $e){
+    $displayErrorMsg = "something went wrong";
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Freelancer - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Friendly Barber</a>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="contactForm.html">login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead bg-primary text-white text-center">
+      <div class="container">
+        <img class="img-fluid mb-5 d-block mx-auto" src="img/profile.jpg" alt="profile picture" style = "border-radius: 50%; size: 50%;">
+        <h1 class="text-uppercase mb-0">The Phattest</h1>
+        <hr class="star-light">
+        <h2 class="font-weight-light mb-0">Check out my services</h2>
+      </div>
+    </header>
+
+    <!-- service Grid Section -->
+      <div class="container">
+        <h2 class="text-center text-uppercase text-secondary mb-0">Services</h2>
+        <hr class="star-dark mb-5">
+        <div class="row">
+           <?php 
+            while ($row =$sql->fetch()){
+              $displayProductId = $row["product_id"];
+              $displayProductName = $row["product_name"];
+              $displayProductDiscription= $row["product_description"];
+              $displayProductPrice = $row["product_price"];
+            
+          ?>
+
+          <div class="col-md-6 col-lg-4">
+              <div class="serviceContent">
+                <h1><?php echo $displayProductId;?><h1>
+                <h1><?php echo $displayProductName; ?> </h1>
+                <h3><?php echo $displayProductDiscription; ?> </h3>
+                <h3><?php echo $displayProductPrice; ?> </h3>
+                <p class = "serviceBtn" <a class="btn" href ="contactForm.html" role ="button" > Inquire Our Service</a></p>
+              </div>
+          </div>
+
+          <?php 
+              } //close while loop
+              $conn = null;
+            ?>
+          <h3><?php echo $displayErrorMsg; ?></h3>
+          
+        </div>
+      </div>
+        
+
+
+    <!-- Footer -->
+    <footer class="footer text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Location</h4>
+            <p class="lead mb-0">2215 John Daniel Drive
+              <br>Clark, MO 65243</p>
+          </div>
+          <div class="col-md-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Around the Web</h4>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-google-plus"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-linkedin"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fa fa-fw fa-dribbble"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <h4 class="text-uppercase mb-4">About Freelancer</h4>
+            <p class="lead mb-0">It's really a side hustle and hobby.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="copyright py-4 text-center text-white">
+      <div class="container">
+        <small>Copyright &copy; wwww.phatluong.com</small>
+      </div>
+    </div>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-to-top d-lg-none position-fixed ">
+      <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+      </a>
+    </div>
+
+    
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/freelancer.min.js"></script>
+
+  </body>
+
+</html>
